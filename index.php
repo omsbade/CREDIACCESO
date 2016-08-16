b66a102bd54b6ed71bb5fe564a70b6e0642c07bf
Omar Said Balanzar Depraect <packandpack1@gmail.com>
2016-08-16 04:12:31 -0500

    pruebas mas y mas

--- index.php
+++ index.php
@@ -38,29 +38,11 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
-                git 
+            
                 <a class="navbar-brand" href="#page-top">CREDIACCESO</a>
             </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li class="page-scroll">
-                        <a href="#portfolio">Portfolio</a>
-                    </li>
-                    <li class="page-scroll">
-                        <a href="#about">Acerca</a>
-                    </li>
-                    <li class="page-scroll">
-                        <a href="#contact">Contacto</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
+     
         <!-- /.container-fluid -->
     </nav>
 
