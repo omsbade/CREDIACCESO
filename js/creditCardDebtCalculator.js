2e193ed5cada791c91a2e1abeaec2f0c55727322
Omar Said Balanzar Depraect <packandpack1@gmail.com>
2016-08-16 20:41:12 -0500

    borrar

--- js/creditCardDebtCalculator.js
+++ js/creditCardDebtCalculator.js
@@ -1,73 +1,69 @@
-$(function() {
+/* This script and many more are available free online at
+The JavaScript Source!! http://www.javascriptsource.com
+Created by: Chris Crenshaw | http://www.creditcarddebtnegotiation.org/ */
 
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            event.preventDefault();
-            
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+// validation function
+function isValid(entry, a, b) {
+  if (isNaN(entry.value) || (entry.value==null) || (entry.value=="") || (entry.value < a) || (entry.value > b)) {
+  alert("Invalid entry. Your min payment should be between " + a + " and " + b + ".")
+  entry.focus()
+  entry.select()
+  return false
+  }
+  return true
+}
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            });
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
+// clear results fields when input values changed
+function clearCalcs(form) {
+  form.num_months.value = ""
+  form.total_pay.value = ""
+  form.total_int.value = ""
+}
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
+function calculate(form) {
+// send entries to validation function
+// exit if not valid
+  if (!isValid(form.balance, 0, 100000)) {  
+    return false
+  } else if (!isValid(form.interest, 0, 30)) {  
+    return false
+  } else {         
+    var init_bal = eval(form.balance.value);
+  }
+  if (!isValid(form.mnth_pay, init_bal*.02, init_bal)) {
+    return false
+  } else {
+  // variables used in calculation
+  var cur_bal = init_bal;   // used in loop
+  var interest = eval(form.interest.value/100);
+  var mnth_pay = eval(form.mnth_pay.value);
+  var fin_chg = 0;      // finance charge
+  var num_mnths = 0;
+  var tot_int = 0;
+  }
+  
+  while (cur_bal > 0) {
+    fin_chg = cur_bal*interest/12;
+    cur_bal = cur_bal - mnth_pay + fin_chg;
+    num_mnths++;
+      if (num_mnths > 300) {
+        alert("We are interrupting this process to prevent a hang which may result with a very high balance, high interest rate and low monthly payment.\n\nTry entering a higher monthly payment amount." )
+        form.mnth_pay.focus()
+        form.mnth_pay.select()
+        return
+      }
+    tot_int += fin_chg;
+  }
+  
+// display result
+  form.num_months.value = num_mnths;
+  form.total_pay.value ="$" + round(init_bal + tot_int);
+  form.total_int.value ="$" + round(tot_int);
+   }
 
-// When clicking on Full hide fail/success boxes
-$('#name').focus(function() {
-    $('#success').html('');
-});
+// round to 2 decimal places
+function round(x) {
+  return Math.round(x*100)/100;
+}
+
+
